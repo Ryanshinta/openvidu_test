31aab7f77bca15d9d81db1eaf45ded5fce8b7556
Ryanshinta <ryanouyangyunru@gmail.com>
2022-11-23 05:07:35 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import "./OpenVidu/openvidu-webcomponent-2.24.0"
+import "./OpenVidu/openvidu-webcomponent-2.24.0.css"
+import axios from "axios";
+import {useEffect} from "react";
 
 function App() {
+  useEffect(() =>{
+    const webComponent = document.querySelector('openvidu-webcomponent');
+    console.log(webComponent);
+    webComponent.addEventListener('onSessionCreated', (event) =>{
+      const session = event.detail;
+      session.on('connectionCreated', (e) => {
+        console.log("connectionCreated", e);
+      });
+
+      session.on('streamDestroyed', (e) => {
+        console.log("streamDestroyed", e);
+      });
+
+      session.on('streamCreated', (e) => {
+        console.log("streamCreated", e);
+      });
+
+      session.on('sessionDisconnected', (event) => {
+        console.warn("sessionDisconnected event");
+        webComponent.style.display = 'none';
+      });
+
+      session.on('exception', (exception) => {
+        console.error(exception);
+      });
+    });
+
+    webComponent.addEventListener('onJoinButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarLeaveButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarCameraButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarMicrophoneButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarScreenshareButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarParticipantsPanelButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarChatPanelButtonClicked', (event) => { });
+    webComponent.addEventListener('onToolbarFullscreenButtonClicked', (event) => { });
+    webComponent.addEventListener('onParticipantCreated', (event) => { });
+
+
+  },[])
+
+  async function joinSession() {
+    const sessionId = "AAA-BBB-CCC";
+
+    console.warn('SESSION ID', sessionId);
+
+    var promiseResults = await Promise.all([getToken(sessionId), getToken(sessionId)]);
+
+    console.warn('promiseResults', promiseResults);
+
+    var tokens = {webcam: promiseResults[0].token, screen: promiseResults[1].token};
+
+    var webComponent = document.querySelector('openvidu-webcomponent');
+
+    var main = document.getElementById("main");
+    main.style.display = 'none';
+
+    webComponent.tokens = tokens;
+  }
+
+  async function getToken(sessionId) {
+    const id = await createSession(sessionId);
+    return createToken(sessionId);
+
+  }
+
+  async function createToken(sessionId) {
+    console.log("Start createToken " + sessionId)
+    const config = {
+      method: 'post',
+      url: 'http://localhost:4443/openvidu/api/sessions/' + sessionId + '/connection',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + 'MY_SECRET'),
+      }
+    };
+    const res = await axios(config)
+    console.log(res)
+    return res.data
+  }
+
+  function createSession(sessionId) {
+
+     var data = {
+       customSessionId: sessionId
+      }
+
+    const config = {
+      method: 'post',
+      url: 'http://localhost:4443/openvidu/api/sessions',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + 'MY_SECRET'),
+      },
+      data: data
+    };
+    axios(config)
+        .then(function (response) {
+          console.log(response.data);
+          return response.data
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+        <input id={"main"} type={"button"} value={"Join"} onClick={joinSession}/>
+        <openvidu-webcomponent style={{height:"100%"}}></openvidu-webcomponent>
+      </>
+  )
 }
 
 export default App;
